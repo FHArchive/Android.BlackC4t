f493431d3deecc7627543a8e71d0d7df0f939e24
Kieran W <41634689+FredHappyface@users.noreply.github.com>
2019-12-04 00:11:41 +0000

    Migrated to android 29 (q)
    using androidx appcompat

--- app/src/main/java/com/fredhappyface/blackc4t/Abstract_Activity.java
+++ app/src/main/java/com/fredhappyface/blackc4t/Abstract_Activity.java
@@ -3,7 +3,7 @@ package com.fredhappyface.blackc4t;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 public abstract class Abstract_Activity extends AppCompatActivity {
 
